fcce6530506fdd3c0c718a52bc005b8338615c57
17g01acs028 <stevemutioo123@gmail.com>
2024-07-26 02:20:31 +0300

    changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,6 @@
 import axios from 'axios'
 
 const api = axios.create({
-<<<<<<< HEAD
-    baseURL: "https://agrisoko-backend.vercel.app"
-||||||| parent of fa50de7 (latest)
     baseURL: 'https://agrisoko-backend.vercel.app/api'
-=======
-    baseURL: "/api"
->>>>>>> fa50de7 (latest)
 })
 export default api
